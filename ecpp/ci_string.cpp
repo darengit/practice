7c7c3ffe5d849c89903acb789036a494e67f03a3
darengit <darenzou@hotmail.com>
2016-10-17 22:32:51 -0400

    start reading exceptional cpp

---
+++ ecpp/ci_string.cpp
@@ -0,0 +1,44 @@
+#include <assert.h>
+#include <ctype.h>
+#include <string.h>
+
+#include <string>
+
+using namespace std;
+
+namespace ecpp {
+	class ci_char_traits : public char_traits<char> {
+		public:
+			static constexpr bool eq(char a, char b) {
+				return toupper(a)==toupper(b);
+			}
+			static constexpr bool lt(char a, char b) {
+				return toupper(a)<toupper(b);
+			}
+			static int compare(const char *s1, const char *s2, size_t count) {
+				for(size_t i=0; i<count; ++i) {
+					if(lt(s1[i],s2[i]))
+						return -1;
+					if(!eq(s1[i],s2[i]))
+						return 1;
+				}
+				return 0;
+			}
+	};
+
+	typedef basic_string<char, ecpp::ci_char_traits> ci_string;
+}
+
+int main() {
+	ecpp::ci_string s("AbCdE");
+
+	assert(s=="abcde");
+	assert(s=="ABCDE");
+	assert(s<"b");
+	assert(s<"B");
+	assert(s>"ab");
+	assert(s>"Ab");
+
+	assert(strcmp(s.c_str(),"AbCdE") == 0);
+	assert(strcmp(s.c_str(),"abcde") != 0);
+}
