e33f508fbbf80d25df748dc152731a06633af5fe
darengit <darenzou@hotmail.com>
2016-11-05 11:55:25 -0400

    minor changes, working torwards compilation

--- wordcount/tester.cpp
+++ wordcount/tester.cpp
@@ -2,32 +2,32 @@
 #include "tester.h"
 
 void testLinearSearch(WordReader &reader) {
-	std::vector<Item> wordList;
+    std::vector<Item> wordList;
 
-	while(String8 word = reader.getWord()) {
-		int i;
-		for(i=0; i<wordList.size(); ++i) {
-			if(wordList[i].word == word) {
-				++wordList[i].count;
-				break;
-			}
-		}
+    while(String8 word = reader.getWord()) {
+	size_t i;
+	for(i=0; i<wordList.size(); ++i) {
+	    if(wordList[i].word == word) {
+		++wordList[i].count;
+		break;
+	    }
+        }
 
-		if(i>=wordList.size())
-			wordList.emplace_back(Item(word,1));
-	}
+	if(i>=wordList.size())
+	    wordList.emplace_back(Item(word,1));
+    }
 
-	for (const Item &item : wordList)
-		printf("%s: %d\n", item.word.toString().c_str(), item.count);
+    for (const Item &item : wordList)
+        printf("%s: %d\n", item.word.toString().c_str(), item.count);
 }
 
 template <typename T>
 void testMap(WordReader &reader) {
-	T wordCount;
+    T wordCount;
 
-	while(String8 word = reader.getWord())
-		wordCount[word] += 1;
+    while(String8 word = reader.getWord())
+	wordCount[word] += 1;
 
-	for(const std::pair<String8, int> &p : wordCount)
-		printf("%s: %d\n", p.first.toString().c_str(), p.second);
+    for(const std::pair<String8, int> &p : wordCount)
+        printf("%s: %d\n", p.first.toString().c_str(), p.second);
 }
--- wordcount/wordcount.cpp
+++ wordcount/wordcount.cpp
@@ -1,43 +1,44 @@
 #include <stdio.h>
 #include <thread>
+#include <functional>
 #include <chrono>
 #include "string8.h"
 #include "wordreader.h"
 #include "tester.h"
 
 struct TestEntry {
-	void (*testFunc)(WordReader &reader);
-	const char *name;
+    void (*testFunc)(WordReader &reader);
+    const char *name;
 };
 
 TestEntry TestEntries[] = {
-	{ testLinearSearch, "linear search" },
-	{ testMap<std::map<String8, int>>, "std::map" },
-	{ testMap<std::unordered_map<String8, int>>, "std::unordered_map" },
+    { testLinearSearch, "linear search" },
+    { testMap<std::map<String8, int>>, "std::map" },
+    { testMap<std::unordered_map<String8, int>>, "std::unordered_map" },
 };
 
 void tester(TestEntry *entry, char *filename) {
-	std::chrono::time_point<std::chrono::system_clock> start, end;
-	start = std::chrono::high_resolution_clock::now();
-	WordReader reader(filename);
-	entry->testFunc(reader);
-	end = std::chrono::high_resolution_clock::now();
-	auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
-	printf("%s : %dms\n", entry->name, millis);
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::high_resolution_clock::now();
+    WordReader reader(filename);
+    entry->testFunc(reader);
+    end = std::chrono::high_resolution_clock::now();
+    auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
+    printf("%s : %ldms\n", entry->name, millis);
 }
 
 int main(int argc, const char *argv[]) {
-	if(argc < 2) {
-		printf("usage: %s <input_file>\n", argv[0]);
-		return 0;
-	}
-
-	std::thread ts[3];
-	for(int i=0; i<3; ++i)
-		ts[i] = std::thread(tester, TestEntries+i, argv[1]);
-
-	for(int i=0; i<3; ++i)
-		ts[i].join();
-
+    if(argc < 2) {
+	printf("usage: %s <input_file>\n", argv[0]);
 	return 0;
+    }
+
+    std::thread ts[3];
+    for(int i=0; i<3; ++i)
+	ts[i] = std::thread(tester, &TestEntries[i], argv[1]);
+
+    for(int i=0; i<3; ++i)
+	ts[i].join();
+
+    return 0;
 }
