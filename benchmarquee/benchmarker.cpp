3912a444a49794493810b3467d2a8b86390d9cd8
darengit <darenzou@hotmail.com>
2017-02-23 21:51:48 -0500

    allow to run a single func against a single input, to run it against something like perf or valgrind

--- benchmarquee/benchmarker.cpp
+++ benchmarquee/benchmarker.cpp
@@ -1,38 +1,58 @@
 #include <assert.h>
 #include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "benchmarquee.h"
 
 #define MILLIS_PER_SEC 1000
 
-int main() {
+int main(int argc, char *argv[]) {
+    int funcNum=-1;
+    if(argc>1)
+        funcNum = atoi(argv[1]);
+
+    int inputNum=-1;
+    if(argc>2)
+        inputNum = atoi(argv[2]);
+
     clock_t start;
     clock_t end;
 
     initInputs();
     for(int j=0; j<inputSize; ++j) {
 
-        start = clock();
-        RETURN_TYPE correct = benchmarquee[0](benchmarkInputs[j]);
-        end = clock();
-        printf("benchmark %d input %d took %fms\n", 0, j, (double)(end-start)/CLOCKS_PER_SEC*MILLIS_PER_SEC);
+        if(j!=inputNum && inputNum!=-1)
+            continue;
+
+        RETURN_TYPE correct;
+        if(funcNum<=0) {
+            start = clock();
+            correct = benchmarquee[0](benchmarkInputs[j]);
+            end = clock();
+            printf("benchmark %d input %d took %fms\n", 0, j, (double)(end-start)/CLOCKS_PER_SEC*MILLIS_PER_SEC);
+        }
 
         for(int i=1; i<benchmarkLength; ++i) {
-
+ 
+            if(i!=funcNum && funcNum!=-1)
+                continue;
 
             start = clock();
             RETURN_TYPE output = benchmarquee[i](benchmarkInputs[j]);
             end = clock();
             printf("benchmark %d input %d took %fms\n", i, j, (double)(end-start)/CLOCKS_PER_SEC*MILLIS_PER_SEC);
-            assert(checkOutput(correct, output));
+ 
+            if(funcNum==-1)
+                assert(checkOutput(correct, output));
 
             freeOutput(output);
         }
 
         printf("\n");
 
-        freeOutput(correct);
+        if(funcNum<=0)
+            freeOutput(correct);
     }
     freeInputs();
     return 0;
