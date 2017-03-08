d529439b501e73d9bba3b3805fec42285b5ea649
darengit <darenzou@hotmail.com>
2017-03-07 22:15:08 -0500

    benchmarquee : cleanup benchmarker.cpp a bit

--- benchmarquee/benchmarker.cpp
+++ benchmarquee/benchmarker.cpp
@@ -8,49 +8,39 @@
 #define MILLIS_PER_SEC 1000
 
 int main(int argc, char *argv[]) {
-    int funcNum=-1;
-    if(argc>1)
-        funcNum = atoi(argv[1]);
-
-    int inputNum=-1;
-    if(argc>2)
-        inputNum = atoi(argv[2]);
+    int inputNum = (argc>1?atoi(argv[1]):-1);
+    int funcNum = (argc>2?atoi(argv[2]):-1);
 
     clock_t start;
     clock_t end;
 
     initInputs();
     for(int j=0; j<inputSize; ++j) {
-
         if(j!=inputNum && inputNum!=-1)
             continue;
 
-        RETURN_TYPE correct;
-        if(funcNum<=0) {
-            start = clock();
-            correct = benchmarquee[0](benchmarkInputs[j]);
-            end = clock();
-            printf("benchmark %d input %d took %fms\n", 0, j, (double)(end-start)/CLOCKS_PER_SEC*MILLIS_PER_SEC);
-        }
 
-        for(int i=1; i<benchmarkLength; ++i) {
- 
+        RETURN_TYPE correct;
+        for(int i=0; i<benchmarkLength; ++i) {
             if(i!=funcNum && funcNum!=-1)
                 continue;
 
             start = clock();
             RETURN_TYPE output = benchmarquee[i](benchmarkInputs[j]);
             end = clock();
-            printf("benchmark %d input %d took %fms\n", i, j, (double)(end-start)/CLOCKS_PER_SEC*MILLIS_PER_SEC);
- 
-            if(funcNum==-1)
-                assert(checkOutput(correct, output));
+            printf("input %d benchmark %d took %fms\n", j, i, (double)(end-start)/CLOCKS_PER_SEC*MILLIS_PER_SEC);
 
+            if(inputNum==-1 && funcNum==-1) {
+                if(i==0)
+                    correct = output;
+                else
+                    checkOutput(correct, output);
+            }
+ 
             freeOutput(output);
         }
 
         printf("\n");
-
         if(funcNum<=0)
             freeOutput(correct);
     }
